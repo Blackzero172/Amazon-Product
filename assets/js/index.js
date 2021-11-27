db97b03a3d1535f7d1dd8d032e043e56a2028734
BlackZero172 <Supermeega@outlook.com>
2021-11-27 22:05:44 +0200

    Added Shipping Details + Carousel

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,3 +10,30 @@ imgSelectors.forEach((img) => {
 		img.classList.add("selected");
 	});
 });
+let minimized = true;
+const toggleClass = () => {
+	propertiesContainer.classList.toggle("minimized");
+	if (minimized) {
+		minimizeArrow.classList.remove("fa-chevron-down");
+		minimizeArrow.classList.add("fa-chevron-up");
+		minimizeText.innerText = "See less";
+
+		minimized = false;
+	} else {
+		minimizeArrow.classList.remove("fa-chevron-up");
+		minimizeArrow.classList.add("fa-chevron-down");
+		minimizeText.innerText = "See more";
+		minimized = true;
+	}
+};
+let seeMoreBtn = document.querySelector(".see-more-btn");
+let propertiesContainer = document.querySelector(".properties-container");
+let minimizeArrow = document.querySelector(".minimize-arrow");
+let minimizeText = document.querySelector(".minimize-text");
+seeMoreBtn.addEventListener("click", toggleClass);
+
+let addToCartBtn = document.querySelector(".add-to-cart");
+let cartCounter = document.querySelector(".counter");
+addToCartBtn.addEventListener("click", () => {
+	if (+cartCounter.innerText < 9) cartCounter.innerText++;
+});
